2e0344b0e782ad031d151213a0becc4d199fc7e2
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2016-01-22 00:00:37 +0100

    fare, wip

--- transit/model.py
+++ transit/model.py
@@ -125,3 +125,205 @@ class Agency(object):
 
     def __ne__(self, other):
         return not self.__eq__(other)
+
+
+class ServicePeriod(object):
+    pass
+
+
+class Service(object):
+
+    id = None
+
+    @property
+    def uri(self):
+        return '{0}/resource/services/{1}'.format(BASE_URI, self.id)
+
+    @property
+    def resource(self):
+        return '{0}#{1}'.format(BASE_URI, self.__class__.__name__)
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class Trip(object):
+
+    id = None
+    route_id = None
+    service_id = None
+    headsign = None
+
+    @property
+    def uri(self):
+        return '{0}/resource/trips/{1}'.format(BASE_URI, self.id)
+
+    @property
+    def resource(self):
+        return '{0}#{1}'.format(BASE_URI, self.__class__.__name__)
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class FarePricing(object):
+
+    id = None
+    price = None
+    currency = None
+    payment_method = None
+
+    @property
+    def uri(self):
+        return '{0}/resource/farePricings/{1}'.format(BASE_URI, self.id)
+
+    @property
+    def resource(self):
+        return '{0}#{1}'.format(BASE_URI, self.__class__.__name__)
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class FareTransfer(object):
+
+    id = None
+    transfers = None
+    transfer_duration = None
+
+    @property
+    def uri(self):
+        return '{0}/resource/fareTransfers/{1}'.format(BASE_URI, self.id)
+
+    @property
+    def resource(self):
+        return '{0}#{1}'.format(BASE_URI, self.__class__.__name__)
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class Fare(object):
+
+    id = None
+    pricing = None
+    transfer = None
+
+    @property
+    def uri(self):
+        return '{0}/resource/fares/{1}'.format(BASE_URI, self.id)
+
+    @property
+    def resource(self):
+        return '{0}#{1}'.format(BASE_URI, self.__class__.__name__)
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class FareRule(object):
+
+    id = None
+    fare_id = None
+
+
+class ZoneFareRule(FareRule):
+    zone_id = None
+
+    @property
+    def uri(self):
+        return '{0}/resource/zoneFareRules/{1}'.format(BASE_URI, self.id)
+
+    @property
+    def resource(self):
+        return '{0}#{1}'.format(BASE_URI, self.__class__.__name__)
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class RouteFareRule(FareRule):
+    route_id = None
+
+    @property
+    def uri(self):
+        return '{0}/resource/routeFareRules/{1}'.format(BASE_URI, self.id)
+
+    @property
+    def resource(self):
+        return '{0}#{1}'.format(BASE_URI, self.__class__.__name__)
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class ZoneCrossFareRule(FareRule):
+    origin_id = None
+    destination_id = None
+
+    @property
+    def uri(self):
+        return '{0}/resource/zoneCrossFareRules/{1}'.format(BASE_URI, self.id)
+
+    @property
+    def resource(self):
+        return '{0}#{1}'.format(BASE_URI, self.__class__.__name__)
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class Zone(object):
+
+    id = None
+
+    @property
+    def uri(self):
+        return '{0}/resource/zones/{1}'.format(BASE_URI, self.id)
+
+    @property
+    def resource(self):
+        return '{0}#{1}'.format(BASE_URI, self.__class__.__name__)
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+
+
--- transit/transiftread.py
+++ transit/transiftread.py
@@ -1,4 +1,4 @@
-def generate_graph(stops, routes, format='turtle'):
+def generate_graph(stops, routes, trips, fares, fare_rules, format='turtle'):
 
     from rdflib import Graph, URIRef, Literal
     from rdflib import RDF
@@ -34,6 +34,65 @@ def generate_graph(stops, routes, format='turtle'):
         graph.add((URIRef(route.uri), URIRef(schema_uri('url')), Literal(route.url)))
         graph.add((URIRef(route.uri), URIRef(schema_uri('transportType')), URIRef(route.transport_type)))
 
+    for _, trip in trips.items():
+        assert isinstance(trip, model.Trip)
+        graph.add((URIRef(trip.uri), RDF.type, URIRef(trip.resource)))
+        graph.add((URIRef(trip.uri), URIRef(schema_uri('headsign')), Literal(trip.headsign)))
+
+        service = model.Service()
+        service.id = trip.service_id
+        route = model.Route()
+        route.id = trip.route_id
+
+        graph.add((URIRef(trip.uri), URIRef(schema_uri('service')), URIRef(service.uri)))
+        graph.add((URIRef(route.uri), URIRef(schema_uri('trip')), URIRef(trip.uri)))
+
+    for _, fare in fares.items():
+        assert isinstance(fare, model.Fare)
+
+        graph.add((URIRef(fare.uri), RDF.type, URIRef(fare.resource)))
+
+        graph.add((URIRef(fare.transfer.uri), RDF.type, URIRef(fare.transfer.resource)))
+        graph.add((URIRef(fare.transfer.uri), URIRef(schema_uri('transfers')), Literal(fare.transfer.transfers)))
+        graph.add((URIRef(fare.transfer.uri), URIRef(schema_uri('transferDuration')),
+                   Literal(fare.transfer.transfer_duration)))
+
+        graph.add((URIRef(fare.pricing.uri), RDF.type, URIRef(fare.pricing.resource)))
+        graph.add((URIRef(fare.pricing.uri), URIRef(schema_uri('price')), Literal(fare.pricing.price)))
+        graph.add((URIRef(fare.pricing.uri), URIRef(schema_uri('currency')),
+                   Literal(fare.pricing.currency)))
+        graph.add((URIRef(fare.pricing.uri), URIRef(schema_uri('paymentMethod')),
+                   Literal(fare.pricing.payment_method)))
+
+        graph.add((URIRef(fare.transfer.uri), URIRef(schema_uri('fareTransfer')), URIRef(fare.transfer.uri)))
+        graph.add((URIRef(fare.transfer.uri), URIRef(schema_uri('farePricing')), URIRef(fare.pricing.uri)))
+
+    for _, fare_rule in fare_rules.items():
+
+        assert isinstance(fare_rule, model.FareRule)
+
+        fare = model.Fare()
+        fare.id = fare_rule.fare_id
+        graph.add((URIRef(fare_rule.uri), RDF.type, URIRef(fare_rule.resource)))
+        graph.add((URIRef(fare_rule.uri), URIRef(schema_uri('fare')), URIRef(fare.uri)))
+
+        if isinstance(fare_rule, model.RouteFareRule):
+            route = model.Route()
+            route.id = fare_rule.route_id
+            graph.add((URIRef(fare_rule.uri), URIRef(schema_uri('route')), URIRef(route.uri)))
+
+        elif isinstance(fare_rule, model.ZoneFareRule):
+            zone = model.Zone()
+            zone.id = fare_rule.zone_id
+            graph.add((URIRef(fare_rule.uri), URIRef(schema_uri('zone')), URIRef(zone.uri)))
+
+        elif isinstance(fare_rule, model.ZoneFareRule):
+            origin, destination = model.Zone(), model.Zone()
+            origin.id,destination.id = fare_rule.origin_id, fare_rule.destination_id
+
+            graph.add((URIRef(fare_rule.uri), URIRef(schema_uri('originZone')), URIRef(origin.uri)))
+            graph.add((URIRef(fare_rule.uri), URIRef(schema_uri('destinationZone')), URIRef(destination.uri)))
+
     return graph.serialize(format=format)
 
 
@@ -58,6 +117,9 @@ if __name__ == '__main__':
     stops = {}
     agencies = {}
     routes = {}
+    trips = {}
+    fares = {}
+    fare_rules = {}
 
     with open(inp, 'rb') as zfp:
         zip_file = zipfile.ZipFile(zfp)
@@ -105,7 +167,85 @@ if __name__ == '__main__':
 
             routes[route.id] = route
 
-    data = generate_graph(stops=stops, routes=routes)
+        # TODO: revise
+        reader = csv.reader(StringIO.StringIO(zip_file.read('trips.txt')))
+
+        # headers
+        next(reader)
+
+        for line in reader:
+
+            trip = model.Trip()
+            trip.id = line[2]
+            trip.route_id = line[0]
+            trip.service_id = line[1]
+            trip.headsign = line[3]
+
+            trips[trip.id] = trip
+
+        reader = csv.reader(StringIO.StringIO(zip_file.read('fare_attributes.txt')))
+
+        # headers
+        next(reader)
+
+        for line in reader:
+
+            pricing = model.FarePricing()
+            pricing.id = str(uuid.uuid4())
+            pricing.price = line[1]
+            pricing.currency = line[2]
+            pricing.payment_method = 0
+            transfer = model.FareTransfer()
+            transfer.id = str(uuid.uuid4())
+            transfer.transfers = line[4]
+            transfer.transfer_duration = line[5]
+
+            fare = model.Fare()
+            fare.id = line[0]
+            fare.pricing = pricing
+            fare.transfer = transfer
+
+            fares[fare.id] = fare
+
+        reader = csv.reader(StringIO.StringIO(zip_file.read('fare_rules.txt')))
+
+        # headers
+        next(reader)
+
+        for line in reader:
+
+            fare_id, route_id, origin_id, destination_id, contains_id = line
+
+            if not origin_id and not destination_id:
+
+                # zone fare
+                fare_rule = model.ZoneFareRule()
+                fare_rule.fare_id = fare_id
+                fare_rule.zone_id = contains_id
+
+                fare_rules[fare_rule.id] = fare_rule
+
+            if origin_id and destination_id:
+
+                # zone cross fare
+                fare_rule = model.ZoneCrossFareRule()
+                fare_rule.id = str(uuid.uuid4())
+                fare_rule.fare_id = fare_id
+                fare_rule.origin_id = origin_id
+                fare_rule.destination_id = destination_id
+
+                fare_rules[fare_rule.id] = fare_rule
+
+            if route_id:
+
+                fare_rule = model.RouteFareRule()
+                fare_rule.id = str(uuid.uuid4())
+                fare_rule.route_id = route_id
+
+                fare_rules[fare_rule.id] = fare_rule
+
+    data = generate_graph(stops=stops, routes=routes, trips=trips, fares=fares,
+                          fare_rules=fare_rules, format='xml')
 
     with open('data/transit-data.rdf', 'w') as fp:
         fp.write(data)
